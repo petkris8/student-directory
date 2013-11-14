8a18df31df48dca572c03ae5b7d6b4005314936d
petkris8 <petkris8@gmail.com>
2013-11-14 14:34:27 +0000

    added student directory

---
+++ directory.rb
@@ -0,0 +1,21 @@
+# first we print the list of students
+	puts "Jean-Baptiste Blanc"
+	puts "James Brooke"
+	puts "Nicki"
+	puts "Gianni"
+	puts "James Graham"
+	puts "Jeremy"
+	puts "Michael"
+	puts "Nisar"
+	puts "Asta"
+	puts "Ken"
+	puts "Peter"
+# finally print the total
+    print "Overall, we have "
+# it's important that print doesn't add new line characters
+print 9
+puts " great students"
+
+
+
+
