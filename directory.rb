3ec802562bdce88af6ad54a37793112a4ae1631d
petkris8 <petkris8@gmail.com>
2013-11-20 16:42:04 +0000

    added user input

--- directory.rb
+++ directory.rb
@@ -1,23 +1,35 @@
-STUDENTS = { november: [ "Jean-Baptiste", "James Brook",
-	"Nicki", "Gianni", "Jeremy", "Michael", "Nisar",
-	"Asta", "Jack", "Lara", "Chris", "Peter", "Simon"]
-}
+
 
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "--------------"
 end
- 
-def print_students
-  STUDENTS[:november].each do |student|
-  	puts "#{student} (#{STUDENTS.keys.first} cohort)"
+
+def input_students
+  puts "Please enter the names of students"
+  puts "To finish,just hit return twice"
+  # create an empty array
+  students = [ ]
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+  	# add the student hash to the array
+	students << {:name => name, :cohort => :november}
+	puts "Now we have #{students.length} students"
+	# get another name from the user
+    name = gets.chomp
   end
+  # return the array of students
+  students
+
 end
 
-def print_footer
-  puts "Overall, we have #{STUDENTS[:november].length} great students"
-end
 
+students = input_students
 print_header
-print_students
-print_footer
+print(students)
+print_footer(students)
+
+
+
